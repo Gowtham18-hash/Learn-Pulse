a34c2b98e4b41755b8520f9d1185548a22cba321
Gowtham18-hash <gwth2004@gmail.com>
2025-11-28 08:43:59 +0530

    version 8.1

--- src/CourseDetails.jsx
+++ src/CourseDetails.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import './CourseDetails.css'
+import { lazy } from 'react'
 function CourseDetails(props) {
 
     return (
@@ -7,7 +8,7 @@ function CourseDetails(props) {
             <div className="card" >
                 <Link to={`/${props.category}/${props.id}`} style={{ textDecoration: "none", color: "black" }}>
                     <div className='content'>
-                    <img src={props.image} alt="" />
+                    <img loading='lazy' src={props.image} alt="" />
                         <h3 >{props.name}</h3>
                         <h6 >{props.description}</h6>
                         <p >{props.price}</p>
